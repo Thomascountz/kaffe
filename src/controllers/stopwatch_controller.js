fdb971032e3c08861d03c3d62dbced52a72d3a7b
thomascountz <thomascountz@gmail.com>
2019-12-27 22:40:29 -0500

    working stimulus

--- src/controllers/stopwatch_controller.js
+++ src/controllers/stopwatch_controller.js
@@ -1,32 +1,46 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "display" ]
+  static targets = [ "display", "trigger", "indicator" ]
 
   connect() {
-    console.log("Connected to stopwatch controller")
-    console.log(this)
+    this.renderButton()
+    this.renderTime()
   }
 
-  start() {
-    this.startStopwatch(this.now())
-  }
-
-  pause() {
-  }
-
-  reset() {
+  toggle(event) {
+    if (this.isRunning) {
+      this.stop()
+      this.renderButton()
+    } else {
+      this.time = 0
+      this.start(this.now())
+      this.renderButton()
+    }
   }
 
   now() {
     return Math.round(new Date().getTime() / 1000);
   }
 
-  startStopwatch(startedAt) {
+  start(startedAt) {
+    this.isRunning = true
     this.stopWatch = setInterval(() => {
       this.time = this.now() - startedAt
-      console.log(this.time)
     }, 1000);
+  }
+
+  stop() {
+    this.isRunning = false;
+    clearInterval(this.stopWatch)
+  }
+
+  get isRunning() {
+    return this.data.get("isRunning").toLowerCase() === "true"
+  }
+
+  set isRunning(value) {
+    this.data.set("isRunning", value.toString())
   }
 
   get time() {
@@ -35,14 +49,30 @@ export default class extends Controller {
 
   set time(value) {
     this.data.set("time", value)
-    this.displayTime()
+    this.renderTime()
+    this.updateIndicators()
   }
 
-  displayTime() {
+  renderTime() {
     this.displayTargets.forEach((el, i) => {
-      console.log(this.decorateTime((this.time)))
       el.innerHTML = this.decorateTime(this.time)
     });
+  }
+
+  renderButton() {
+    if (this.isRunning) {
+      this.triggerTargets.forEach((el, i) => {
+        el.innerHTML = "Stop Brew"
+        el.classList.remove("btn-outline-primary")
+        el.classList.add("btn-outline-danger")
+      })
+    } else {
+      this.triggerTargets.forEach((el, i) => {
+        el.innerHTML = "Start Brew"
+        el.classList.remove("btn-outline-danger")
+        el.classList.add("btn-outline-primary")
+      })
+    }
   }
 
   decorateTime(timeInSeconds) {
@@ -51,5 +81,22 @@ export default class extends Controller {
     return `${minutes.toString()}:${seconds.toString().padStart(2, "0")}`;
   }
 
+  updateIndicators() {
+    if (this.indicatorTargets.some((indicator) => {
+      return indicator.dataset.stepAt == this.time.toString()
+    })) {
+      this.indicatorTargets.forEach((indicator, i) => {
+        if (parseInt(indicator.dataset.stepAt) == this.time) {
+          indicator.classList.add("list-group-item-success")
+        } else if (parseInt(indicator.dataset.stepAt) < this.time) {
+          indicator.classList.remove("list-group-item-success")
+          indicator.classList.add("list-group-item-secondary")
+        } else {
+          indicator.classList.remove("list-group-item-success")
+          indicator.classList.remove("list-group-item-secondary")
+        }
+      })
+    }
+  }
 }
 
